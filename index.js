4a61dd72f1fbb798ec19f8908e4c63c64659b04d
Rawshanara Eity <rawshanara.eity@gmail.com>
2024-05-20 23:47:38 +0600

    added crud operation

---
+++ index.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const app = express()
+const cors = require('cors')
+
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+//  middleware
+app.use(cors());
+app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bkpsd7x.mongodb.net/?retryWrites=true&w=majority`;
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    const taskCollection = client.db('taskManagerDB').collection('tasks')
+
+
+    app.post('/tasks', async(req, res) =>{
+        const item = req.body;
+        const result = await taskCollection.insertOne(item)
+        res.send(result)
+    })
+
+    app.get('/tasks', async(req, res) =>{
+        const result = await taskCollection.find().toArray();
+        res.send(result)
+    })
+
+
+
+    app.delete('/tasks/:id', async(req, res) =>{
+        const id = req.params.id
+        const query = {_id:new ObjectId(id)}
+        const result = await taskCollection.deleteOne(query)
+        res.send(result)
+    })
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+app.get('/', (req,res) =>{
+    res.send('task manager is running')
+})
+
+app.listen(port, () =>{
+    console.log(`task manager is running on ${port}`);
+})
